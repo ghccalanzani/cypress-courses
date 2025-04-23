3b211efaac133a128acf84b1a165bf897a179763
Gustavo Calanzani <ghc.calanzani@gmail.com>
2025-04-23 14:44:44 -0300

    ajustes

--- 1-Cypress-Zero-a-Nuvem/cypress/e2e/teste7.cy.js
+++ 1-Cypress-Zero-a-Nuvem/cypress/e2e/teste7.cy.js
@@ -1,7 +1,27 @@
 /// <reference types="Cypress" />
 
 describe('Exercicio 7', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+      cy.visit('./website/index.html')
   })
+
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade')
+    .should('have.attr', 'href', 'privacy.html')
+    .and('have.attr', 'target', '_blank')
+  })
+
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+    .invoke('removeAttr', 'target')
+    .click()
+
+    cy.contains('Não salvamos dados submetidos no formulário da aplicação CAC TAT.').should('be.visible')
+  })
+
+  it('testa a página da política de privacidade de forma independente', () => {
+    cy.visit('./website/privacy.html')
+    cy.contains('p', 'Talking About Testing').should('be.visible')
+  })
+
 })
